31b8d45a63ee08fdc44538747206a1044844c7af
WoodenShard <298028119+WoodenShard@users.noreply.github.com>
2026-07-07 21:21:16 +0200

    Understanding of surfaces in py game

--- code/main.py
+++ code/main.py
@@ -4,7 +4,12 @@ import pygame
 pygame.init() # always to start pygame
 WINDOW_WIDTH, WINDOW_HEIGHT= 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) # needs a tuple so doudle (())
+pygame.display.set_caption('Space shooter')
 running : bool = True
+
+# surface
+
+surf = pygame.Surface((100,200)) # creates a surface once again using a tuple
 
 while running:
     # event loop
@@ -14,7 +19,8 @@ while running:
 
 
     # draw game
-    display_surface.fill('red')
+    display_surface.fill('darkgray')
+    display_surface.blit(surf, (100,150)) # to display our surface we use this which takes 2 params 1) what to display 2) x,y of where to
     pygame.display.update() # updates all the screen
 
 pygame.quit() # making sure it closes correctly since u used pygame.init() 

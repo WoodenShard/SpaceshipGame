489fa43705f4e2a11b17c56d4e97a5d97b62b1ca
WoodenShard <298028119+WoodenShard@users.noreply.github.com>
2026-07-09 14:12:21 +0200

    Adding the meteor class and making them move diagonally

--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 import pygame
-from random import randint
+from random import randint,uniform
 
 class Player(pygame.sprite.Sprite):
     
@@ -11,7 +11,7 @@ class Player(pygame.sprite.Sprite):
         self.image= pygame.image.load('images/player.png').convert_alpha()
         self.rect: pygame.FRect= self.image.get_frect(center=(window_width / 2, window_height / 2 + 300))
         self.direction = pygame.math.Vector2(0,0)
-        self.speed = 300
+        self.speed = 600
 
         # cooldown
 
@@ -49,21 +49,32 @@ class Star(pygame.sprite.Sprite):
         self.rect = self.image.get_frect(center =(randint(0,window_width),randint(0,window_height)))
 
 class Meteor(pygame.sprite.Sprite):
-    def __init__(self, groups):
+    def __init__(self,surf,pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('images/meteor.png').convert_alpha()
-        self.rect = self.image.get_frect(center= (window_width/2, window_height/2))
+        self.image = surf
+        self.rect: pygame.FRect = self.image.get_frect(center = pos)
+        self.direction = pygame.math.Vector2(uniform(-0.5,0.5),1)
+        self.speed: int = randint(400,500)
+    def update(self,dt):
+        self.rect.center += 400 * dt * self.direction
+        if self.rect.top > window_height:
+            self.kill()
+        
 
 class Laser(pygame.sprite.Sprite):
     def __init__(self,surf,pos,groups):
         super().__init__(groups)
         self.image = surf
         self.rect: pygame.FRect = self.image.get_frect(midbottom = pos)
+        self.direction = pygame.math.Vector2()
+
 
     def update(self,dt):
-        self.rect.centery -= 400 * dt  
+        self.rect.centery -= 200 * dt
+        if self.rect.bottom <0:
+            self.kill()
 
-laser_surf = pygame.image.load('images/laser.png')
+
 
 pygame.init()
 
@@ -73,26 +84,33 @@ pygame.display.set_caption('Space Shooters')
 running : bool = True
 clock = pygame.time.Clock() # it can control the frame rate
 
+
+laser_surf = pygame.image.load('images/laser.png').convert_alpha()
+meteor_surf = pygame.image.load('images/meteor.png').convert_alpha()
+
+
 all_sprites = pygame.sprite.Group() # groups can draw update and organize the stripes
 player = Player(all_sprites)
 for i in range(20):
     Star(all_sprites)
-meteor = Meteor(all_sprites)
+
 
 # meteor event
 
 meteor_event = pygame.event.custom_type()
-pygame.time.set_timer(meteor_event, 2000)
+pygame.time.set_timer(meteor_event, 200)
 
 
 
 while running:
     dt = clock.tick() / 1000 # decides the framerate
+    position_meteor = pygame.math.Vector2((randint(0,1280),randint(-200,-100)))
     # checking for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == meteor_event:
+            meteor = Meteor(meteor_surf,position_meteor,all_sprites)
             pass
   
     all_sprites.update(dt)

79395f920bef113c9ea7198f86dc4a8457085845
WoodenShard <298028119+WoodenShard@users.noreply.github.com>
2026-07-10 01:20:11 +0200

    Added collision between meteors and player and between meteors and lasers and started on making the game look better

--- code/main.py
+++ code/main.py
@@ -1,5 +1,5 @@
 from typing import Any
-
+from os import path
 import pygame
 from random import randint,uniform
 
@@ -17,7 +17,7 @@ class Player(pygame.sprite.Sprite):
 
         self.can_shoot: bool = True
         self.laser_shoot_time: int = 0
-        self.cooldown_duration: int= 1000
+        self.cooldown_duration: int= 600
 
     def laser_timer(self):
         if not self.can_shoot:
@@ -35,7 +35,7 @@ class Player(pygame.sprite.Sprite):
 
         recentKeys = pygame.key.get_just_pressed()
         if recentKeys[pygame.K_SPACE] and self.can_shoot:
-            Laser(laser_surf,self.rect.midtop,all_sprites)
+            Laser(laser_surf,self.rect.midtop,(all_sprites, laser_sprites))
             self.can_shoot: bool = False
             self.laser_shoot_time = pygame.time.get_ticks()
 
@@ -70,11 +70,20 @@ class Laser(pygame.sprite.Sprite):
 
 
     def update(self,dt):
-        self.rect.centery -= 200 * dt
+        self.rect.centery -= 400 * dt
         if self.rect.bottom <0:
             self.kill()
+ 
+def collisions():
+    global running
+    collision_sprites = pygame.sprite.spritecollide(player, meteor_sprites, True)
+    if collision_sprites:
+        running = False 
 
-
+    for laser in laser_sprites:
+        collided_sprite = pygame.sprite.spritecollide(laser,meteor_sprites,True)
+        if collided_sprite:
+            laser.kill()
 
 pygame.init()
 
@@ -84,12 +93,17 @@ pygame.display.set_caption('Space Shooters')
 running : bool = True
 clock = pygame.time.Clock() # it can control the frame rate
 
+# import
+laser_surf = pygame.image.load(path.join('images','laser.png')).convert_alpha()
+meteor_surf = pygame.image.load(path.join('images','meteor.png')).convert_alpha()
+font = pygame.font.Font(path.join('images','Oxanium-Bold.ttf'),20)
+text_surf = font.render('dsfs',True, 'red')
 
-laser_surf = pygame.image.load('images/laser.png').convert_alpha()
-meteor_surf = pygame.image.load('images/meteor.png').convert_alpha()
 
-
-all_sprites = pygame.sprite.Group() # groups can draw update and organize the stripes
+# sprites
+all_sprites: pygame.sprite.Group = pygame.sprite.Group() # groups can draw update and organize the stripes
+meteor_sprites: pygame.sprite.Group = pygame.sprite.Group()
+laser_sprites: pygame.sprite.Group = pygame.sprite.Group()
 player = Player(all_sprites)
 for i in range(20):
     Star(all_sprites)
@@ -98,26 +112,26 @@ for i in range(20):
 # meteor event
 
 meteor_event = pygame.event.custom_type()
-pygame.time.set_timer(meteor_event, 200)
-
-
+pygame.time.set_timer(meteor_event, 400)
 
 while running:
     dt = clock.tick() / 1000 # decides the framerate
-    position_meteor = pygame.math.Vector2((randint(0,1280),randint(-200,-100)))
+    
     # checking for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == meteor_event:
-            meteor = Meteor(meteor_surf,position_meteor,all_sprites)
-            pass
+            position_meteor = pygame.math.Vector2((randint(0,1280),randint(-200,-100)))
+            Meteor(meteor_surf,position_meteor,(all_sprites,meteor_sprites))
   
     all_sprites.update(dt)
+    collisions()
             
     # Drawing game
     window.fill('darkgray')
 
+    window.blit(text_surf,(0,0))
     all_sprites.draw(window)
 
     # updating the screen
